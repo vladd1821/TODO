17b07690925118f3493eb94b8499525a44ad9d8c
vladd1821 <david071521@mail.ru>
2025-03-17 18:35:08 +0300

    add TaskServiceTest.java

--- src/test/java/com/davidenko/TODO/service/TaskServiceTest.java
+++ src/test/java/com/davidenko/TODO/service/TaskServiceTest.java
@@ -18,6 +18,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -80,22 +81,62 @@ class TaskServiceTest {
         assertEquals(1, taskDTOList.size());
         assertEquals("Test Task", taskDTOList.get(0).getTaskName());
         verify(taskRepository, times(1)).findAll();
+        verify(modelMapper, times(1)).map(task, TaskDTO.class);
 
     }
 
     @Test
-    void getAllTasksByStatus() {
+    void getAllTasksByStatus_shouldReturnAllTasksByStatus() {
+        when(taskRepository.findAllByStatus(Status.IN_PROGRESS)).thenReturn(List.of(task));
+        when(modelMapper.map(task, TaskDTO.class)).thenReturn(taskDTO);
+
+        List<TaskDTO> taskDTOList = taskService.getAllTasksByStatus(Status.IN_PROGRESS);
+
+        assertEquals(1,taskDTOList.size());
+        assertEquals("IN_PROGRESS",taskDTOList.get(0).getStatus().toString());
+        verify(taskRepository,times(1)).findAllByStatus(Status.IN_PROGRESS);
+        verify(modelMapper, times(1)).map(task, TaskDTO.class);
     }
 
     @Test
-    void deleteTaskById() {
+    void deleteTaskById_shouldCallMethod1Times() {
+        taskService.deleteTaskById(1L);
+
+        verify(taskRepository,times(1)).deleteById(1L);
     }
 
     @Test
-    void save() {
+    void save_shouldSavePerson() {
+        when(modelMapper.map(taskDTO, Task.class)).thenReturn(task);
+
+        taskService.save(taskDTO);
+
+        verify(modelMapper, times(1)).map(taskDTO, Task.class);
+        verify(taskRepository, times(1)).save(task);
+        assertNotNull(task);
     }
 
     @Test
-    void partlyUpdateTaskByTaskId() {
+    public void testPartlyUpdateTaskByTaskId() throws Exception {
+        when(taskRepository.findById(1L)).thenReturn(Optional.of(task));
+        when(modelMapper.map(task, TaskDTO.class)).thenReturn(taskDTO);
+
+        taskDTO.setStatus(Status.IN_PROGRESS);
+
+        taskService.partlyUpdateTaskByTaskId(1L, taskDTO);
+        verify(taskRepository, times(1)).save(task);
+        assertEquals("IN_PROGRESS", task.getStatus().toString());
+    }
+
+    @Test
+    public void testPartlyUpdateTaskByTaskId_NotFound() {
+        when(taskRepository.findById(1L)).thenReturn(Optional.empty());
+
+        Throwable exception = assertThrows(Exception.class, () -> {
+            taskService.partlyUpdateTaskByTaskId(1L, taskDTO);
+        });
+
+        assertEquals("task not found, id: 1", exception.getMessage().toString());
+        verify(taskRepository, times(0)).save(any());
     }
 }
